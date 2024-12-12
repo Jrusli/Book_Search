a9acc8c9d13399ea815b7d577854db664dce12e3
Joshua R <111922365+Jrusli@users.noreply.github.com>
2024-12-12 01:57:00 -0800

    Update login.php

--- login.php
+++ login.php
@@ -3,15 +3,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $csvFilePath = 'C:\xampp\htdocs\Library_Search\user.csv'; // Update this path to the location of your CSV file
+    $csvFilePath = 'C:\xampp\htdocs\Library_Search\user.csv';
 
     if (file_exists($csvFilePath)) {
         $file = fopen($csvFilePath, 'r');
-        fgetcsv($file); // Skip the header row
+        fgetcsv($file);
 
         $isValidUser = false;
         while (($row = fgetcsv($file)) !== FALSE) {
-            if ($row[0] === $username && $row[1] === $password) { // Compare plain text passwords
+            if ($row[0] === $username && $row[1] === $password) { 
                 $isValidUser = true;
                 break;
             }
